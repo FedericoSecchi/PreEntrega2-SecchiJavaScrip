931508df300c6a71869f77a904869da57fecde13
FedericoSecchi <federico.secchimarino@gmail.com>
2023-03-30 13:28:24 -0400

    push

---
+++ stock.js
@@ -0,0 +1,56 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Cometa Delta",
+        tipo: "cometa",
+        descripcion: "Una cometa delta con excelente estabilidad",
+        precio: 3500,
+        cantidad: 10
+    },
+
+    {
+        id: 2,
+        nombre: "Cometa Stunt",
+        tipo: "cometa",
+        descripcion: "Una cometa stunt para realizar trucos en el aire",
+        precio: 5000,
+        cantidad: 5
+    },
+
+    {
+        id: 3,
+        nombre: "Tabla Kitesurf",
+        tipo: "kitesurf",
+        descripcion: "Una tabla de kitesurf con gran flotabilidad",
+        precio: 25000,
+        cantidad: 3
+    },
+
+    {
+        id: 4,
+        nombre: "Arnes",
+        tipo: "accesorio",
+        descripcion: "Un arnes de kitesurf para mayor comodidad",
+        precio: 5000,
+        cantidad: 7
+    },
+
+    {
+        id: 5,
+        nombre: "Traje de Neopreno",
+        tipo: "accesorio",
+        descripcion: "Un traje de neopreno para kitesurf",
+        precio: 15000,
+        cantidad: 4
+    },
+
+    {
+        id: 6,
+        nombre: "Barra de Kite",
+        tipo: "accesorio",
+        descripcion: "Una barra de kite para mayor control de la cometa",
+        precio: 8000,
+        cantidad: 6
+    }
+];
